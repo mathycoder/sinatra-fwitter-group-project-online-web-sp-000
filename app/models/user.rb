a94912001cbe8013ce6bc82b6d7d30c881b174b5
Adam S <adam.sarli@gmail.com>
2019-05-13 19:34:12 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def slug
-    
+    self.name.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|instance| instance.slug == slug}
   end
 end
