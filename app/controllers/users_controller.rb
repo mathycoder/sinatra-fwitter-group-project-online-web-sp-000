5027e9aabd4a28762567228fab2771510a765677
Adam S <adam.sarli@gmail.com>
2019-05-13 19:24:47 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,13 +41,4 @@ class UsersController < ApplicationController
 
   end
 
-  helpers do
-    def logged_in?
-      !session[:user_id].nil?
-    end
-
-    def current_user
-      User.find(session[:user_id])
-    end
-  end
 end
