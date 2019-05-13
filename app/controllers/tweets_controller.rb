027ff37a5b7dde096a880ae34a3dc4c897257337
Adam S <adam.sarli@gmail.com>
2019-05-13 18:56:55 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,38 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  get '/tweets' do
+    @user = User.find(session[:user_id])
+    erb :'tweets/index'
+  end
+
+  post '/tweets' do
+    tweet = Tweet.create(params)
+    redirect "/tweets/#{tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    tweet.update(content: params[:content])
+    redirect "/tweets/#{tweet.id}"
+  end
+
+  delete '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !session[:user_id].nil?
+      redirect '/tweets'
+    end
+    erb :'users/create_user'
+  end
 
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    end
+    user = User.create(params)
+    session[:user_id] = user.id
+    redirect '/tweets'
+  end
 end
