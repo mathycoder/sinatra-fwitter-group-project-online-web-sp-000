e5c8ff2bcb93409e72f9fbe05063a8a4583cbf3e
Adam S <adam.sarli@gmail.com>
2019-05-13 19:06:14 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,14 +21,12 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    binding.pry
     user = User.find_by(username: params[:username]).authenticate(params[:password])
     if user
       session[:user_id] = user.id
       redirect '/tweets'
     else
       redirect '/login'
-    end 
-
+    end
   end
 end
