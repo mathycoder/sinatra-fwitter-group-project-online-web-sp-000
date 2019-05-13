3d32ced49ad375677346657fe50d11e32e9f9488
Adam S <adam.sarli@gmail.com>
2019-05-13 20:04:56 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,9 +28,9 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id' do
-    if !logged_in
+    if !logged_in?
       redirect "/login"
-    end 
+    end
     @tweet = Tweet.find(params[:id])
     erb :'tweets/show_tweet'
   end
