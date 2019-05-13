27b382df1d27c65b7540bed2781bd0c4b0b988a3
Adam S <adam.sarli@gmail.com>
2019-05-13 19:49:50 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,11 @@
 class TweetsController < ApplicationController
 
   get '/tweets/new' do
-    erb :'tweets/new'
+    if logged_in?
+      erb :'tweets/new'
+    else 
+      redirect '/login'
+    end 
   end
 
   get '/tweets' do
